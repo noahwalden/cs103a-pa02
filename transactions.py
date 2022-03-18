e19bcd0c526fab94a199c0a11ebc3dc42410400a
Noah Walden <noahwalden@cs.brandeis.edu>
2022-03-17 23:58:39 -0400

    Create Transactions class

---
+++ transactions.py
@@ -0,0 +1,12 @@
+import sqlite3
+
+class Transactions():
+
+    def __init__(self, db_filename):
+        self.c = db_filename
+        con= sqlite3.connect(db_filename)
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE IF NOT EXISTS transactions
+                    ('item #' int, amount int, category text, date text, description text)''')
+        con.commit()
+        con.close()
